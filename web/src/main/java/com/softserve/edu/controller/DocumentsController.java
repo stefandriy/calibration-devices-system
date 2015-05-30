6af26da551edacfd1bf95b2d05107d9321a8e8e6
oleh.chernygevych <oleh.chernygevych@gmail.com>
2015-05-30 02:48:55 -0700

    fix forced base64 encoding

--- web/src/main/java/com/softserve/edu/controller/DocumentsController.java
+++ web/src/main/java/com/softserve/edu/controller/DocumentsController.java
@@ -5,16 +5,15 @@ import com.softserve.edu.documents.parameter.DocumentType;
 import com.softserve.edu.service.DocumentsService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.beans.PropertyEditorSupport;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Controller for file generation requests.
@@ -25,88 +24,106 @@ import java.io.*;
 @RestController
 @RequestMapping(value = "/doc")
 public class DocumentsController {
-
     @Autowired
     DocumentsService documentsService;
 
     /**
-     * Returns a document with a specific format using verification and one of it's tests.
+     * Returns a document with a specific documentFormat using verification and one of it's tests.
      * For example: .../verification_certificate/1/1/pdf.
      *
-     * @param documentType document to generate
+     * @param documentType     document to generate
      * @param verificationCode id of the verification, for which the document is to be generated
-     * @param testID one of the verification's tests, for which the document is to be generated
-     * @param format format of the resulting document
-     * @return the generated document
-     * @throws IOException if file can't be generated because of a file system error
+     * @param testID           one of the verification's tests, for which the document is to be generated
+     * @param documentFormat   documentFormat of the resulting document
+     * @throws IOException           if file can't be generated because of a file system error
      * @throws IllegalStateException if one of parameters is incorrect
      */
-    @RequestMapping(value = "{documentType}/{verificationCode}/{testID}/{format}", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> getDocument(@PathVariable DocumentType documentType,
-                                              @PathVariable String verificationCode,
-                                              @PathVariable Long testID,
-                                              @PathVariable DocumentFormat format)
+    @RequestMapping(value = "{documentType}/{verificationCode}/{testID}/{documentFormat}", method = RequestMethod.GET)
+    public void getDocument(HttpServletResponse response,
+                            @PathVariable DocumentType documentType,
+                            @PathVariable String verificationCode,
+                            @PathVariable Long testID,
+                            @PathVariable DocumentFormat documentFormat)
             throws IOException, IllegalStateException {
         // check input parameters
-        InputStream outputStream = documentsService.getFile(verificationCode, testID, documentType, format);
-
-        byte[] bytes = IOUtils.toByteArray(outputStream);
-        outputStream.close();
-
-        String fileName;
-
-        if (format == DocumentFormat.PDF) {
-            fileName = "the.pdf";
-        } else {
-            fileName = "the.docx";
-        }
-
-        FileOutputStream out = new FileOutputStream(fileName);
-        out.write(bytes);
-        out.close();
-
-        ResponseEntity<byte[]> responseEntity = makeResponse(bytes, HttpStatus.OK, format);
-
-        return responseEntity;
+        InputStream inputStream = documentsService.getFile(verificationCode, testID, documentType, documentFormat);
+        sendFile(response, documentFormat, inputStream);
+        response.getOutputStream().close();
+        inputStream.close();
     }
 
     /**
-     * Returns a document with a specific format using verification that has only one test.
+     * Returns a document with a specific documentFormat using verification that has only one test.
      * For example: .../verification_certificate/1/pdf.
      *
-     * @param documentType document to generate
+     * @param documentType     document to generate
      * @param verificationCode id of the verification, for which the document is to be generated. This verification
-     *                       must have only one test
-     * @param format format of the resulting document
-     * @return the generated document
-     * @throws IOException if file can't be generated because of a file system error
+     *                         must have only one test
+     * @param documentFormat   documentFormat of the resulting document
+     * @throws IOException           if file can't be generated because of a file system error
      * @throws IllegalStateException if one of parameters is incorrect
      */
-    @RequestMapping(value = "{documentType}/{verificationCode}/{format}", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> getDocument(@PathVariable DocumentType documentType,
-                                              @PathVariable String verificationCode,
-                                              @PathVariable DocumentFormat format)
+    @RequestMapping(value = "{documentType}/{verificationCode}/{documentFormat}", method = RequestMethod.GET)
+    public void getDocument(HttpServletResponse response,
+                            @PathVariable DocumentType documentType,
+                            @PathVariable String verificationCode,
+                            @PathVariable DocumentFormat documentFormat)
             throws IOException, IllegalStateException {
-        InputStream outputStream = documentsService.getFile(verificationCode, documentType, format);
+        InputStream inputStream = documentsService.getFile(verificationCode, documentType, documentFormat);
+        sendFile(response, documentFormat, inputStream);
+        response.getOutputStream().close();
+        inputStream.close();
+    }
 
-        byte[] bytes = IOUtils.toByteArray(outputStream);
-        outputStream.close();
+    /**
+     * Writes contents of the input stream to the response' output stream and sets http headers depending on
+     * the documentFormat.
+     * Doesn't close the streams.
+     *
+     * @param response       servlet response
+     * @param documentFormat of the file to be sent
+     * @param inputStream    input steam to the data that must be written to the output stream
+     * @throws IOException
+     */
+    private void sendFile(HttpServletResponse response, DocumentFormat documentFormat, InputStream inputStream) throws IOException {
+        setContentType(response, documentFormat);
+        writeToOutputStream(inputStream, response.getOutputStream());
+    }
 
-//        String fileName;
-//
-//        if (format == DocumentFormat.PDF) {
-//            fileName = "the.pdf";
-//        } else {
-//            fileName = "the.docx";
-//        }
-//
-//        FileOutputStream out = new FileOutputStream(fileName);
-//        out.write(bytes);
-//        out.close();
+    /**
+     * Set content type of the response depending on the document's format.
+     *
+     * @param response       servlet response
+     * @param documentFormat of the file to be sent
+     */
+    private void setContentType(HttpServletResponse response, DocumentFormat documentFormat) {
+        switch (documentFormat) {
+            case PDF:
+                response.setContentType("application/pdf");
+                break;
+            case DOCX:
+                response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+                break;
+        }
 
-        ResponseEntity<byte[]> responseEntity = makeResponse(bytes, HttpStatus.OK, format);
+        response.setHeader("Content-Disposition", "attachment; filename=\"myfile." +
+                documentFormat.name().toLowerCase() +
+                "\"");
+        //request.setContentLength(bytes.length);
+    }
 
-        return responseEntity;
+    /**
+     * Writes contents of the input stream to the output stream
+     *
+     * @param inputStream  to the data to be written
+     * @param outputStream to write data to
+     * @throws IOException if one of streams is unreachable
+     */
+    private void writeToOutputStream(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        inputStream.close();
+
+        outputStream.write(bytes);
     }
 
     /**
@@ -169,42 +186,5 @@ public class DocumentsController {
                 setValue(documentType);
             }
         });
-    }
-
-    /**
-     * Creates a response.
-     *
-     * @param responseParameter parameter of the response
-     * @param httpStatus http status of the respond
-     * @param <T> type of the response parameter
-     * @return response that is ready to be sent
-     */
-    private <T> ResponseEntity<T> makeResponse(T responseParameter, HttpStatus httpStatus,
-                                               DocumentFormat documentFormat) {
-        Assert.notNull(responseParameter, "Response parameter = " + responseParameter + ". " +
-                "Response parameter can't be null");
-        HttpHeaders headers = new HttpHeaders();
-
-        String filename;
-
-        switch (documentFormat) {
-            case PDF:
-                headers.setContentType(MediaType.parseMediaType("application/pdf"));
-                filename = "output.pdf";
-                break;
-            case DOCX:
-                headers.setContentType(MediaType.parseMediaType("application/docx"));
-                filename = "output.docx";
-                break;
-            default:
-                throw new IllegalArgumentException(documentFormat.name() + "is not supported");
-        }
-        headers.setContentDispositionFormData(filename, filename);
-//        headers.setContentDispositionFormData("attachment", filename);
-//        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-//        headers.add("Content-Type", "application/docx; charset=utf-8");
-        headers.add("Content-Transfer-Encoding ", "");
-
-        return new ResponseEntity<>(responseParameter, headers, httpStatus);
     }
 }
