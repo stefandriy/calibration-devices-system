50b1131bf5411f6a755d15d8f2f25aa80d6eb8d6
Sander1992 <vyblov92@gmail.com>
2015-05-31 22:04:59 +0300

    add calibrator

---
+++ web/src/main/webapp/resources/app/calibrator/calibratorModule.js
@@ -0,0 +1,53 @@
+(function () {
+    angular.module('calibratorModule', ['spring-security-csrf-token-interceptor',
+        'ui.bootstrap', 'ui.router', 'ui.bootstrap.showErrors'])
+
+        .config(['$stateProvider', '$urlRouterProvider', 'showErrorsConfigProvider',
+
+            function ($stateProvider, $urlRouterProvider, showErrorsConfigProvider) {
+
+                showErrorsConfigProvider.showSuccess(true);
+
+                $urlRouterProvider.otherwise('/');
+
+                $stateProvider
+                    .state('main-panel', {
+                        url: '/',
+                        templateUrl: '/resources/app/calibrator/views/main-panel.html'
+                    })
+                    .state("new-verifications", {
+                        url: '/verifications/new',
+                        templateUrl: '/resources/app/calibrator/views/new-verifications.html',
+                        controller: 'NewVerificationsController'
+                    })
+                    .state("adding-verifications", {
+                        url: '/verifications/add',
+                        templateUrl: '/resources/app/calibrator/views/adding-verifications.html',
+                        controller: 'AddingVerificationsController'
+                    })
+                    .state("verifications-archive", {
+                        url: '/verifications/archive',
+                        templateUrl: '/resources/app/calibrator/views/archival-verifications.html',
+                        controller: 'ArchivalVerificationsController'
+                    });
+
+            }]);
+
+    angular.module('calibratorModule').run(function (paginationConfig) {
+        paginationConfig.firstText = 'Перша';
+        paginationConfig.previousText = 'Попередня';
+        paginationConfig.nextText = 'Наступна';
+        paginationConfig.lastText = 'Остання';
+    });
+
+    define([
+        'controllers/TopNavBarController',
+        'controllers/MainPanelController',
+        'controllers/ArchivalVerificationsController',
+        'controllers/NewVerificationsController',
+        'controllers/AddingVerificationsController',
+        'controllers/DetailsModalController',
+        'controllers/SendingModalController',
+        'services/VerificationService'
+    ], function () {});
+})();
