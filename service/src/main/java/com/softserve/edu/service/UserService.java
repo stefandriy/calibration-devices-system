5db5bebca8945d11e17a3ad40503ba3db81019f7
misha <mentegy@gmail.com>
2015-06-19 23:23:15 +0300

    add changePassword method

--- service/src/main/java/com/softserve/edu/service/UserService.java
+++ service/src/main/java/com/softserve/edu/service/UserService.java
@@ -1,7 +1,10 @@
 package com.softserve.edu.service;
 
+import com.softserve.edu.entity.user.User;
 import com.softserve.edu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +14,34 @@ public class UserService {
     private UserRepository userRepository;
 
     /**
-     * Check whereas user with {@code username} exist in database
+     * Checks whereas user with {@code username} exist in database
      *
      * @param username must not be non {@literal null}
      * @return {@literal true} if user with {@code username} doesn't exist in database, else {@literal false}
      */
     public boolean existsWithUsername(String username) {
         return userRepository.findOne(username) == null;
+    }
+
+    /**
+     * Changes user's password
+     *
+     * @param username user identification
+     * @param oldPassword old password
+     * @param newPassword new password
+     * @return {@literal true} if changed, if not or passwords don't match - {@literal false}
+     */
+    public boolean changePassword(String username, String oldPassword, String newPassword) {
+        boolean changed = false;
+        User user = userRepository.findOne(username);
+        if (user != null) {
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            changed = passwordEncoder.matches(oldPassword, user.getPassword());
+            if (changed) {
+                user.setPassword(passwordEncoder.encode(newPassword));
+                userRepository.save(user);
+            }
+        }
+        return changed;
     }
 }
