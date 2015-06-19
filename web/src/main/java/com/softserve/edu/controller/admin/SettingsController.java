a01995b032a2d2d2fc17cca12d3d9ef2e86014ac
misha <mentegy@gmail.com>
2015-06-19 23:24:31 +0300

    add Settings controller (with implemented change password method)

---
+++ web/src/main/java/com/softserve/edu/controller/admin/SettingsController.java
@@ -0,0 +1,37 @@
+package com.softserve.edu.controller.admin;
+
+import com.softserve.edu.dto.NewPasswordDTO;
+import com.softserve.edu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping(value = "/admin/settings/")
+public class SettingsController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Changes user's password
+     *
+     * @param newPasswordDTO container for new and old password
+     * @param userDetails user details stored in session
+     * @return 200 OK if changed, else 404 NOT FOUND
+     */
+    @RequestMapping(value = "password", method = RequestMethod.PUT)
+    public ResponseEntity changePassword(
+            @RequestBody NewPasswordDTO newPasswordDTO,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        if (userService.changePassword(
+                userDetails.getUsername(),
+                newPasswordDTO.getOldPassword(),
+                newPasswordDTO.getNewPassword())) {
+            httpStatus = HttpStatus.OK;
+        }
+        return new ResponseEntity(httpStatus);
+    }
+}
