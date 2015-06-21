39e3a12d107c4039c8331fc58547f54d33b01f6b
misha <mentegy@gmail.com>
2015-06-21 14:12:55 +0300

    user settings: implemented changing first/middle/last name

--- service/src/main/java/com/softserve/edu/service/UserService.java
+++ service/src/main/java/com/softserve/edu/service/UserService.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.service;
 
+import com.softserve.edu.entity.user.Employee;
 import com.softserve.edu.entity.user.User;
 import com.softserve.edu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,24 +25,62 @@ public class UserService {
     }
 
     /**
+     * Changes given type of user's name
+     *
+     * @param username must not be non {@literal null}
+     * @param newName new name to change
+     * @param type (first, last or middle name)
+     * @return {@literal true} if changed, if not or passwords don't match - {@literal false}
+     */
+    public boolean changeName(String username, String newName, String type) {
+        boolean isChanged = false;
+        if (type != null && username != null && newName != null) {
+            User user = userRepository.findOne(username);
+            if (user instanceof Employee) {
+                Employee employee = (Employee) user;
+                switch (type) {
+                    case "firstName":
+                        employee.setFirstName(newName);
+                        isChanged = true;
+                        break;
+                    case "lastName":
+                        employee.setLastName(newName);
+                        isChanged = true;
+                        break;
+                    case "middleName":
+                        employee.setMiddleName(newName);
+                        isChanged = true;
+                        break;
+                }
+                if (isChanged) {
+                    userRepository.save(employee);
+                }
+            }
+        }
+        return isChanged;
+    }
+
+    /**
      * Changes user's password
      *
-     * @param username user identification
+     * @param username must not be non {@literal null}
      * @param oldPassword old password
      * @param newPassword new password
      * @return {@literal true} if changed, if not or passwords don't match - {@literal false}
      */
     public boolean changePassword(String username, String oldPassword, String newPassword) {
-        boolean changed = false;
-        User user = userRepository.findOne(username);
-        if (user != null) {
-            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            changed = passwordEncoder.matches(oldPassword, user.getPassword());
-            if (changed) {
-                user.setPassword(passwordEncoder.encode(newPassword));
-                userRepository.save(user);
+        boolean isChanged = false;
+        if (username != null && oldPassword != null && newPassword != null) {
+            User user = userRepository.findOne(username);
+            if (user != null) {
+                PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                isChanged = passwordEncoder.matches(oldPassword, user.getPassword());
+                if (isChanged) {
+                    user.setPassword(passwordEncoder.encode(newPassword));
+                    userRepository.save(user);
+                }
             }
         }
-        return changed;
+        return isChanged;
     }
 }
