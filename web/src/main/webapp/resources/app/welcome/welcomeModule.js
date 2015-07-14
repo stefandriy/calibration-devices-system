2c87a148dfa0f847e7fe1e5ef3f067e79bc5f8d7
mateysh <mateysh7@gmail.com>
2015-07-14 17:52:26 +0300

    Create visual widget which show real-time verification position.Create select input with search option,fix status info massages,add second input form for phone with show/hide checkbox.

--- web/src/main/webapp/resources/app/welcome/welcomeModule.js
+++ web/src/main/webapp/resources/app/welcome/welcomeModule.js
@@ -1,65 +1,83 @@
-(function () {
-    angular
-        .module('welcomeModule', ['spring-security-csrf-token-interceptor',
-            'ui.bootstrap', 'pascalprecht.translate', 'ngCookies', 'ui.router',
-            'ui.bootstrap.showErrors', 'ngTable'])
+(function() {
+	angular
+			.module(
+					'welcomeModule',
+					[ 'spring-security-csrf-token-interceptor', 'ui.bootstrap',
+							'pascalprecht.translate', 'ngCookies', 'ui.router',
+							'ui.bootstrap.showErrors', 'ngTable' ,'localytics.directives','frapontillo.bootstrap-switch'])
 
-        .config(['$translateProvider', '$stateProvider', '$urlRouterProvider', 'showErrorsConfigProvider',
+			.config(
+					[
+							'$translateProvider',
+							'$stateProvider',
+							'$urlRouterProvider',
+							'showErrorsConfigProvider',
 
-            function ($translateProvider, $stateProvider, $urlRouterProvider, showErrorsConfigProvider) {
+							function($translateProvider, $stateProvider,
+									$urlRouterProvider,
+									showErrorsConfigProvider) {
+								
+								showErrorsConfigProvider.showSuccess(true);
 
-                showErrorsConfigProvider.showSuccess(true);
+								/**
+								 * i18n configuration.
+								 */
+								$translateProvider.useStaticFilesLoader({
+									prefix : '/resources/assets/i18n/welcome-',
+									suffix : '.json'
+								});
+								$translateProvider.useLocalStorage();
+								$translateProvider
+										.useSanitizeValueStrategy('escaped');
+								$translateProvider.preferredLanguage('ukr');
+								/**
+								 * Routing configuration.
+								 */
+								$urlRouterProvider.otherwise('/start');
 
-                /**
-                 *  i18n configuration.
-                 */
-                $translateProvider.useStaticFilesLoader({
-                    prefix: '/resources/assets/i18n/welcome-',
-                    suffix: '.json'
-                });
-                $translateProvider.useLocalStorage();
-                $translateProvider.useSanitizeValueStrategy('escaped');
-                $translateProvider.preferredLanguage('ukr');
-                /**
-                 * Routing configuration.
-                 */
-                $urlRouterProvider.otherwise('/start');
+								$stateProvider
+										.state(
+												'start',
+												{
+													url : '/start',
+													templateUrl : '/resources/app/welcome/views/start.html'
+												})
+										.state(
+												'about',
+												{
+													url : '/about',
+													templateUrl : '/resources/app/welcome/views/about.html'
+												})
+										.state(
+												'login',
+												{
+													url : '/login',
+													templateUrl : '/resources/app/welcome/views/login.html',
+													controller : 'LoginController'
+												})
+										.state(
+												'application-sending',
+												{
+													url : '/application-sending',
+													templateUrl : '/resources/app/welcome/views/application-sending.html',
+													controller : 'ApplicationSendingController'
+												})
+										.state(
+												'application-status',
+												{
+													url : '/application-status/{clientCode}',
+													templateUrl : '/resources/app/welcome/views/application-status.html',
+													controller : 'ApplicationStatusController'
+												});
+							} ]);
 
-                $stateProvider
-                    .state('start', {
-                        url: '/start',
-                        templateUrl: '/resources/app/welcome/views/start.html'
-                    })
-                    .state('about', {
-                        url: '/about',
-                        templateUrl: '/resources/app/welcome/views/about.html'
-                    })
-                    .state('login', {
-                        url: '/login',
-                        templateUrl: '/resources/app/welcome/views/login.html',
-                        controller: 'LoginController'
-                    })
-                    .state('application-sending', {
-                        url: '/application-sending',
-                        templateUrl: '/resources/app/welcome/views/application-sending.html',
-                        controller: 'ApplicationSendingController'
-                    })
-                    .state('application-status', {
-                        url: '/application-status/{clientCode}',
-                        templateUrl: '/resources/app/welcome/views/application-status.html',
-                        controller: 'ApplicationStatusController'
-                    });
-            }]);
-
-    define([
-        'controllers/LoginController',
-        'controllers/ApplicationSendingController',
-        'controllers/ApplicationStatusController',
-        'controllers/InternationalizationController',
-        'controllers/NavigationController',
-        'controllers/DetailsController',
-        'services/DataReceivingService',
-        'services/DataSendingService',
-        'directives/OnStartupMessage'
-    ], function () {});
+	define([ 'controllers/LoginController',
+			'controllers/ApplicationSendingController',
+			'controllers/ApplicationStatusController',
+			'controllers/InternationalizationController',
+			'controllers/NavigationController',
+			'controllers/DetailsController', 'services/DataReceivingService',
+			'services/DataSendingService', 'directives/OnStartupMessage' ],
+			function() {
+			});
 })();
