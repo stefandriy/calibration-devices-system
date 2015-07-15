339d9747e75174b9ce34b7a4309240f5ba310ed0
Roman <roman.chmelyck@gmail.com>
2015-07-15 17:58:25 +0300

    code clean-up

--- web/src/main/webapp/resources/app/provider/controllers/NotificationsController.js
+++ web/src/main/webapp/resources/app/provider/controllers/NotificationsController.js
@@ -11,22 +11,22 @@ angular
 	    		$rootScope.$broadcast('refresh-table');
 	    	}
 			
-//	    	$scope.startPolling = function(){
-//					$scope.stopPolling();
-//					if(angular.isDefined(promiseInterval)) return;
-//					promiseInterval = $interval(function () {
-//						verificationService.getCountOfNewVerifications().success(function (count) {
-//				       		$scope.countOfUnreadVerifications = count;
-//							})
-//					}, 5000); 
-//			}
-//
-//	    	$scope.stopPolling = function() {
-//	    		$interval.cancel(promiseInterval);
-//	    	};
-//	    	
-//	    	$scope.startPolling();
-//	    	
+	    	$scope.startPolling = function(){
+					$scope.stopPolling();
+					if(angular.isDefined(promiseInterval)) return;
+					promiseInterval = $interval(function () {
+						verificationService.getCountOfNewVerifications().success(function (count) {
+				       		$scope.countOfUnreadVerifications = count;
+							})
+					}, 5000); 
+			}
+
+	    	$scope.stopPolling = function() {
+	    		$interval.cancel(promiseInterval);
+	    	};
+	    	
+	    	$scope.startPolling();
+	    	
 			$rootScope.$on('verification-sent-to-calibrator', function(){
 				verificationService.getCountOfNewVerifications().success(function (count) {
 			       		$scope.countOfUnreadVerifications = count;
@@ -39,8 +39,8 @@ angular
 					});
 			});
 		
-//			$scope.$on('$destroy', function () {
-//					$scope.stopPolling();
-//			}); 
+			$scope.$on('$destroy', function () {
+					$scope.stopPolling();
+			}); 
 
 	}]);
