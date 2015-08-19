c88a2a996b7e4d0896d0ac247f999fcd7a5d4881
oknan94 <oleg_kos94@ukr.net>
2015-08-19 12:29:56 +0300

    MeasuringEquipmentServiceTest.java (completed) by Oleh Kosar

--- service/src/test/java/com/softserve/edu/service/MeasuringEquipmentServiceTest.java
+++ service/src/test/java/com/softserve/edu/service/MeasuringEquipmentServiceTest.java
@@ -9,10 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
 
 import com.softserve.edu.entity.MeasuringEquipment;
 import com.softserve.edu.repository.MeasuringEquipmentRepository;
 import com.softserve.edu.service.MeasuringEquipmentService;
+import org.springframework.data.domain.*;
 
 import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
@@ -21,29 +23,52 @@ import static org.mockito.Mockito.*;
 public class MeasuringEquipmentServiceTest {
 	@InjectMocks
 	private MeasuringEquipmentService mes;
-	
+
 	@Mock
 	MeasuringEquipmentRepository measuringEquipmentRepository;
 	
+	@Mock
+	MeasuringEquipment meseq;
+	
 	@Before
-	public void init()
-	{
+	public void init() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
+	// for deleteMeasuringEquipment
 	@Test
 	public void test1() {
-		MeasuringEquipment me=new MeasuringEquipment(123L,"Equi","Samsung");
+		MeasuringEquipment me = new MeasuringEquipment(123L, "Equi", "Samsung");
 		when(measuringEquipmentRepository.findOne(123L)).thenReturn(me);
-		MeasuringEquipment got=mes.deleteMeasuringEquipment(123L);
-		assertEquals(me,got);
+		MeasuringEquipment got = mes.deleteMeasuringEquipment(123L);
+		assertEquals(me, got);
 	}
-	
+
+	// for deleteMeasuringEquipment
 	@Test
-	public void test2(){
+	public void test2() {
 		ArgumentCaptor<Long> par1 = ArgumentCaptor.forClass(Long.class);
 		mes.deleteMeasuringEquipment(123L);
 		verify(measuringEquipmentRepository).findOne(par1.capture());
-		assertEquals((Long)123L, par1.getValue());
+		assertEquals((Long) 123L, par1.getValue());
+	}
+
+	// for getMeasuringEquipmentById
+	@Test
+	public void test3() {
+		MeasuringEquipment me = new MeasuringEquipment(123L, "Equi", "Samsung");
+		when(measuringEquipmentRepository.findOne(123L)).thenReturn(me);
+		MeasuringEquipment got = mes.getMeasuringEquipmentById(123L);
+		assertEquals(me, got);
+	}
+
+	// for editMeasuringEquipment
+	@Test
+	public void test4() {
+		when(measuringEquipmentRepository.findOne(123L)).thenReturn(meseq);
+		ArgumentCaptor<String> par1 = ArgumentCaptor.forClass(String.class);
+		mes.editMeasuringEquipment(123L, "name", "type", "mfctrer", "30 days");
+		verify(meseq).setVerificationInterval(par1.capture());
+		assertEquals("30 days", par1.getValue());
 	}
 }
