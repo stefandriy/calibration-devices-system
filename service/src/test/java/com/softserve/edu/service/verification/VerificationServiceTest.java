0c4468794b4ecf7c1e2527866cee6a1bd48c1b11
Volodya20 <ladykkevych@gmail.com>
2015-08-28 10:40:28 +0300

    updated tests

--- service/src/test/java/com/softserve/edu/service/verification/VerificationServiceTest.java
+++ service/src/test/java/com/softserve/edu/service/verification/VerificationServiceTest.java
@@ -1,25 +1,19 @@
 package com.softserve.edu.service.verification;
 
-import static org.junit.Assert.*;
-
+import com.softserve.edu.entity.Verification;
+import com.softserve.edu.repository.CalibrationTestRepository;
+import com.softserve.edu.repository.VerificationRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.data.domain.Page;
 import org.springframework.security.access.AccessDeniedException;
 
-import com.softserve.edu.entity.Verification;
-import com.softserve.edu.repository.CalibrationTestRepository;
-import com.softserve.edu.repository.VerificationRepository;
-import org.springframework.data.domain.*;
-
-import org.mockito.ArgumentCaptor;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VerificationServiceTest {
@@ -44,18 +38,12 @@ public class VerificationServiceTest {
 	public void test1() {
 		when(verificationRepository.findOne("123")).thenReturn(new Verification());
 		Verification get=verificationService.findById("123");
-		assertEquals(get,new Verification()); 
+		assertEquals(get, new Verification());
 	}
 	
-	@Test
+	@Test(expected = AccessDeniedException.class)
 	public void test2() {
 		when(calibrationTestRepository.findById(-1L)).thenReturn(null);
-		try{
-			verificationService.findByCalibrationTestId(-1L);
-		}
-		catch(AccessDeniedException exc){
-			System.out.println("Exception was thrown");
-		}
+		verificationService.findByCalibrationTestId(-1L);
 	}
-
 }
