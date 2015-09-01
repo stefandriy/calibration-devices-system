d7a18b272bd86dd8e2b7251e8fae4089f0a4a6e7
Nazariy Melnychuk <nazariy.melnychuk9@gmail.com>
2015-09-01 16:23:42 +0300

    Extended chosen directive. Added styles for ng-table

--- web/src/main/webapp/resources/app/employeeModule.js
+++ web/src/main/webapp/resources/app/employeeModule.js
@@ -121,6 +121,20 @@
         paginationConfig.lastText = 'Остання';
     });
 
+
+    angular.module('employeeModule').directive('chosen', function () {
+        return {
+            priority: 1,
+            restrict: 'A',
+            link: {
+                pre: function (scope, element, attr, ngModel) {
+                    var defaultText = attr.placeholder;
+                    angular.element(element[0]).attr('data-placeholder', defaultText);
+                }
+            }
+        }
+    });
+
     define([
             'provider/controllers/InternationalizationController',
             'provider/controllers/TopNavBarControllerProvider',
