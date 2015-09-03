644e250c21d4d3f74263e8e30ca21c238624921b
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2015-09-03 23:27:31 +0300

    +type column without sorting and filtering

--- web/src/main/java/com/softserve/edu/controller/admin/util/OrganizationPageDTOTransformer.java
+++ web/src/main/java/com/softserve/edu/controller/admin/util/OrganizationPageDTOTransformer.java
@@ -2,9 +2,12 @@ package com.softserve.edu.controller.admin.util;
 
 import com.softserve.edu.dto.admin.OrganizationPageDTO;
 import com.softserve.edu.entity.Organization;
+import com.softserve.edu.entity.OrganizationType;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by vova on 03.09.15.
@@ -13,11 +16,18 @@ public class OrganizationPageDTOTransformer {
     public static List<OrganizationPageDTO> toDtoFromList(List<Organization> list){
         List<OrganizationPageDTO> resultList = new ArrayList<OrganizationPageDTO>();
         for (Organization organization : list) {
+            Set<OrganizationType> organizationTypes = organization.getOrganizationTypes();
+            List<String> listOrganizationTypes = new ArrayList<String>();
+            organizationTypes.forEach(organizationType -> listOrganizationTypes.add(organizationType.getType()));
+            String[] arrayOrganizationTypes = new String[listOrganizationTypes.size()];
+            arrayOrganizationTypes = listOrganizationTypes.toArray(arrayOrganizationTypes);
+
             resultList.add(new OrganizationPageDTO(
                             organization.getId(),
                             organization.getName(),
-                            organization.getEmail(),
-                            organization.getPhone()
+                    organization.getEmail(),
+                    arrayOrganizationTypes,
+                    organization.getPhone()
             ));
         }
         return resultList;
