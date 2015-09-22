84e0c426618ecf3e9cd91773c06baac8141fc770
nazarii Ivashkiv <nazarii.ivashkiv>
2015-09-22 20:29:00 +0300

    add organization type in script

--- service/src/main/java/com/softserve/edu/service/catalogue/LocalityService.java
+++ service/src/main/java/com/softserve/edu/service/catalogue/LocalityService.java
@@ -2,6 +2,7 @@ package com.softserve.edu.service.catalogue;
 
 import com.softserve.edu.entity.catalogue.Locality;
 import com.softserve.edu.repository.catalogue.LocalityRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +12,16 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class LocalityService {
+    Logger logger = Logger.getLogger(LocalityService.class);
 
     @Autowired
     private LocalityRepository localityRepository;
 
     public List<Locality> getLocalitiesCorrespondingDistrict(Long districtId) {
-        return localityRepository.findDistinctByDistrictId(districtId) ;
+        return localityRepository.findDistinctByDistrictId(districtId);
     }
-    public List<String> getMailIndexForLocality(String designation,Long districtId) {
-        return localityRepository.findMailIndexByDesignationAndDistrictId(designation,districtId);
+
+    public List<String> getMailIndexForLocality(String designation, Long districtId) {
+        return localityRepository.findMailIndexByDesignationAndDistrictId(designation, districtId);
     }
 }
--- web/src/main/java/com/softserve/edu/controller/catalogue/LocalityController.java
+++ web/src/main/java/com/softserve/edu/controller/catalogue/LocalityController.java
@@ -3,6 +3,7 @@ package com.softserve.edu.controller.catalogue;
 import com.softserve.edu.dto.application.ApplicationFieldDTO;
 import com.softserve.edu.controller.client.application.util.CatalogueDTOTransformer;
 import com.softserve.edu.service.catalogue.LocalityService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 public class LocalityController {
+    Logger logger = Logger.getLogger(LocalityController.class);
 
     @Autowired
     private LocalityService localityService;
@@ -21,9 +23,10 @@ public class LocalityController {
     public List<ApplicationFieldDTO> getLocalitiesCorrespondingDistrict(@PathVariable Long districtId) {
         return CatalogueDTOTransformer.toDto(localityService.getLocalitiesCorrespondingDistrict(districtId));
     }
+
     @RequestMapping(value = "application/localities/{locationDesignation}/{districtId}", method = RequestMethod.GET)
-    public List<String> getMailIndexForLocality(@PathVariable String locationDesignation,@PathVariable Long districtId) {
-        return (localityService.getMailIndexForLocality(locationDesignation,districtId));
+    public List<String> getMailIndexForLocality(@PathVariable String locationDesignation, @PathVariable Long districtId) {
+        return (localityService.getMailIndexForLocality(locationDesignation, districtId));
     }
 
 }
