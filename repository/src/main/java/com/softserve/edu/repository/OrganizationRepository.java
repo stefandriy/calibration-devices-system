e8e7c4a6ec61105b78b8d580d8c6fbc5d4cb9c8f
nazarii Ivashkiv <nazarii.ivashkiv>
2015-10-03 16:27:14 +0300

    add queries for organization repo

--- repository/src/main/java/com/softserve/edu/repository/OrganizationRepository.java
+++ repository/src/main/java/com/softserve/edu/repository/OrganizationRepository.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.repository;
 
+import com.softserve.edu.entity.enumeration.device.DeviceType;
 import com.softserve.edu.entity.enumeration.organization.OrganizationType;
 import com.softserve.edu.entity.organization.Organization;
 import org.springframework.data.domain.Page;
@@ -33,15 +34,45 @@ public interface OrganizationRepository extends CrudRepository<Organization, Lon
             "WHERE org.district = :district AND  :typeId in elements(org.organizationTypes)")
     List<Organization> findByDistrictAndType(@Param("district") String district, @Param("type") OrganizationType type);*/
 
+    /**
+     *
+     * @param organizationId
+     * @return
+     */
     @Query("SELECT elements(org.organizationTypes) FROM Organization org WHERE org.id=:organizationId")
     Set<OrganizationType> findOrganizationTypesById(@Param("organizationId") Long organizationId);
 
+    /**
+     * FInd all organizations in selected location by
+     * @param localityId
+     * @return
+     */
     @Query("SELECT o FROM Organization o INNER JOIN o.localities l WHERE l.id=:localityId")
     List<Organization> findOrganizationByLocalityId(@Param("localityId") Long localityId);
 
+    /**
+     * Find all organizations in selected locality and organization type
+     *
+     * @param localityId
+     * @param typeId
+     * @return
+     */
     @Query("SELECT org FROM Organization org " +
             "INNER JOIN org.localities l " +
             "WHERE l.id=:localityId AND  :typeId in elements(org.organizationTypes)")
     List<Organization> findOrganizationByLocalityIdAndType(@Param("localityId") Long localityId, @Param("typeId") OrganizationType typeId);
+
+    /**
+     * Find all organizations in selected locality, organization type and device type
+     *
+     * @param localityId  id of locality
+     * @param orgType  type of organization
+     * @param deviceType  device type
+     * @return list of organizations
+     */
+    @Query("SELECT org FROM Organization org " +
+            "INNER JOIN org.localities l " +
+            "WHERE l.id=:localityId AND  :orgType in elements(org.organizationTypes) AND :deviceType in elements(org.deviceTypes)")
+    List<Organization> findByLocalityIdAndTypeAndDevice(@Param("localityId") Long localityId, @Param("orgType") OrganizationType orgType, @Param("deviceType") DeviceType deviceType );
 
 }
