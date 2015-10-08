097f72f570dece10813d07a84e956f35a218bce6
Volodya20 <ladykkevych@gmail.com>
2015-10-08 13:20:25 +0300

    add integration testing configuration and create tests
     for UserService and ProviderEmployeeService

---
+++ service/src/main/java/com/softserve/edu/config/ServiceTestingConfig.java
@@ -0,0 +1,11 @@
+package com.softserve.edu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPATestingConfig.class)
+@ComponentScan("com.softserve.edu")
+public class ServiceTestingConfig {}
+
