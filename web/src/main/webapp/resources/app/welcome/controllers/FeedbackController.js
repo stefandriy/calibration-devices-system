0da087c414b12ccd88e39e7d6b087a6f9cf3c795
Nazariii <nazarii.ivashkiv@gmail.com>
2015-10-14 07:41:34 +0300

    Added popup notifications after creating and editing organization

--- web/src/main/webapp/resources/app/welcome/controllers/FeedbackController.js
+++ web/src/main/webapp/resources/app/welcome/controllers/FeedbackController.js
@@ -1,7 +1,7 @@
 angular
     .module('welcomeModule')
-    .controller('FeedbackController', ['$scope', '$modalInstance', '$log',
-        function ($scope, $modalInstance, $log) {
+    .controller('FeedbackController', ['$scope', '$modalInstance',
+        function ($scope, $modalInstance) {
             $scope.formData = {};
             $scope.complete = false;
 
@@ -19,7 +19,7 @@ angular
                     $modalInstance.close($scope.formData, $scope.sendingStarted);
 
                 }
-            }
+            };
 
             $scope.FIRST_LAST_NAME_REGEX = /^([A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}\u002d{1}[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}|[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20})$/;
             $scope.EMAIL_REGEX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
