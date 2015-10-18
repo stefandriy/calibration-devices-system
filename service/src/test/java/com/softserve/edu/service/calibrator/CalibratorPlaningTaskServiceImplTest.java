6e1c94aff2ff98a7b43c53f2c246e3c81e43f9bf
yurijdvornyk <yurijdvornyk@gmail.com>
2015-10-18 19:45:02 +0300

    CalibratorPlaningTaskServiceImpl tests added.

---
+++ service/src/test/java/com/softserve/edu/service/calibrator/CalibratorPlaningTaskServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.softserve.edu.service.calibrator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Yurko on 18.10.2015.
+ */
+public class CalibratorPlaningTaskServiceImplTest {
+
+    @Test
+    public void testAddNewTask() throws Exception {
+
+    }
+
+    @Test
+    public void testFindVerificationsByCalibratorIdAndReadStatus() throws Exception {
+
+    }
+}
