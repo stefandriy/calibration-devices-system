629ed8e3e57a0fc25e1e15c61bead30d1e3d8809
stefandriy <stefandriy@gmail.com>
2015-10-20 01:46:54 +0300

    some updates to OrganizationServiceImplTest

--- service/src/test/java/com/softserve/edu/service/admin/impl/OrganizationServiceImplTest.java
+++ service/src/test/java/com/softserve/edu/service/admin/impl/OrganizationServiceImplTest.java
@@ -1,13 +1,15 @@
 package com.softserve.edu.service.admin.impl;
 
 import com.softserve.edu.entity.Address;
+import com.softserve.edu.entity.catalogue.Locality;
 import com.softserve.edu.entity.enumeration.device.DeviceType;
 import com.softserve.edu.entity.enumeration.organization.OrganizationType;
 import com.softserve.edu.entity.organization.Organization;
-import com.softserve.edu.entity.user.User;
+import com.softserve.edu.entity.organization.OrganizationChangesHistory;
+import com.softserve.edu.repository.OrganizationChangesHistoryRepository;
 import com.softserve.edu.repository.OrganizationRepository;
-import com.softserve.edu.repository.UserRepository;
-import com.softserve.edu.service.tool.impl.MailServiceImpl;
+import com.softserve.edu.service.catalogue.LocalityService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -20,226 +22,165 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.stub;
 
+/**
+ * Created by cam on 19.10.15.
+ */
 public class OrganizationServiceImplTest {
+    private final Long organizationId = 1L;
+    private final Long localityId = 2L;
+    private final OrganizationType orgType = OrganizationType.CALIBRATOR;
+    private final DeviceType deviceType = DeviceType.WATER;
+    private final List<Organization> organizationList = new ArrayList<>();
+    private final Set<OrganizationType> organizationTypes = new HashSet<>();
+    private final List<OrganizationChangesHistory> historyList = new ArrayList<>();
+    private final int employeesCapasity = 6;
+
+    @Mock
+    private OrganizationRepository organizationRepository;
+
+    @Mock
+    private Organization organization;
+
+    @Mock
+    private OrganizationChangesHistoryRepository organizationChangesHistoryRepository;
+
+    @Mock
+    private LocalityService localityService;
+
+    @Mock
+    private Locality locality;
+
     @InjectMocks
-    private OrganizationServiceImpl organizationsService;
+    private OrganizationServiceImpl organizationService;
 
-    @Mock
-    private Organization mockOrganization = mock(Organization.class);
-
-    @Mock
-    private User mockUser = mock(User.class);
-
-    @Mock
-    MailServiceImpl mockMail = mock(MailServiceImpl.class);
-
-    @Mock
-    private OrganizationRepository mockOrganizationRepository;
-
-    @Mock
-    private UserRepository mockUserRepository;
 
     @Before
-    public void init() {
+    public void setUp() throws Exception {
+        organizationService = new OrganizationServiceImpl();
         MockitoAnnotations.initMocks(this);
-        when(organizationsService.findOrganizationTypesById(1L)).thenReturn(new HashSet<>(1));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        organizationService = null;
     }
 
     @Test
     public void testAddOrganizationWithAdmin() throws Exception {
-      final String name = "Eric";
-        final String email = "dot@gmail.com";
-        final String phone = "0934453246";
-        List<String> types = new ArrayList<>();
+        final String OrgName = "name";
+        final String mname = "mname";
+        final String phone = "99999999";
+        final String email = "email";
+        final List<String> types = new ArrayList<String>();
+        final List<String> counters = new ArrayList<String>();
         types.add("PROVIDER");
-        types.add("CALIBRATOR");
-        final Integer employeesCapacity = 12;
-        final Integer maxProcessTime = 123;
-        final String username = "eric";
-        final String password = "root";
+        counters.add("WATER");
+        final Integer employeesCapacity = 13;
+        final Integer maxProcessTime = 456;
         final Address address = new Address("Lviv", "Leva", "123", "123", "123", "123");
-        final String firstName = "firstName";
-        final String lastName = "lastName";
-        final String middleName = "middleName";
-        final String adminName = "admin";
-        final Long[] localityIdList = {1L};
+        final Long[] localityIdList = {123L};
+        final String username = "eric123";
+        final String password = "root";
+        String firstName = "firstName";
+        String lastName = "lastName";
+        String middleName = "middleName";
+        String adminName = "admin";
 
-//        organizationsService.addOrganizationWithAdmin(name, email, phone, types, employeesCapacity,
-//                maxProcessTime, firstName, lastName, middleName,
-//                username, password, address, adminName, localityIdList);
+        stub(localityService.findById(anyLong())).toReturn(locality);
 
-//        ArgumentCaptor<Organization> organizationArg = ArgumentCaptor.forClass(Organization.class);
-//        ArgumentCaptor<User> employeeAdminArg = ArgumentCaptor.forClass(User.class);
-//
-//        verify(mockOrganizationRepository).save(organizationArg.capture());
-//        verify(mockUserRepository).save(employeeAdminArg.capture());
-//
-//        assertEquals(userRole, mockUserRepository.getUserRole(anyString()));
-//        assertEquals(typeTwo, mockOrganizationRepository.getOrganizationType(anyString()));
-//        assertEquals(spyOrg.getName(), organizationArg.getValue().getName());
-//        assertEquals(spyUser.getUsername(), employeeAdminArg.getValue().getUsername());
+//        Organization expected = new Organization(OrgName, email, phone, employeesCapacity, maxProcessTime, address);
+
+        organizationService.addOrganizationWithAdmin(OrgName, email, phone, types, employeesCapacity, maxProcessTime,
+                firstName, lastName, middleName, username, password, address, adminName, localityIdList);
+//        organizationService.getOrganizationById(organizationService.findAllByLocalityId(localityIdList[0]).get(0).getId());
     }
 
-
-/*    @Test
+    @Test
     public void testGetOrganizationsBySearchAndPagination() throws Exception {
-        int pageNumber = 5;
+        int pageNumber = 2;
         int itemsPerPage = 10;
         String name = "name";
         String email = "email";
-        String number = "1234567890";
-        String type = "type";
+        String number = "0000000000";
+        String type = "PROVIDER";
         String region = "Lv";
-        String district = "St";
-        String locality = "locality";
+        String district = "Lviv";
+        String locality = "Lviv";
         String streetToSearch = "Shev";
-        String sortCriteria = "sortCriteria";
-        String sortOrder = "asc";
-        ListToPageTransformer<Organization> page = mock(ListToPageTransformer.class);
-        PageRequest pageRequest = new PageRequest(pageNumber - 1, itemsPerPage);
-
-        when(mockOrganizationRepository.findAll(pageRequest)).thenReturn(pageOrganizations);
-        when(mockOrganizationRepository.findByNameLikeIgnoreCase("%" + search
-                + "%", pageRequest)).thenReturn(pageOrganizations);
-        ArgumentCaptor<PageRequest> pageRequestArg = ArgumentCaptor.forClass(PageRequest.class);
+        String sortCriteria = null;
+        String sortOrder = null;
 
 
-        organizationsService.getOrganizationsBySearchAndPagination(pageNumber, itemsPerPage, name, email, number, type, region, district,
-                locality, streetToSearch, sortCriteria, sortOrder);
-
-        verify(mockOrganizationRepository).findAll(pageRequestArg.capture());
-        assertEquals(pageRequest.first(), pageRequestArg.getValue().first());
-        assertEquals(pageOrganizations, organizationsService
-                .getOrganizationsBySearchAndPagination(pageNumber, itemsPerPage, null));
-
-        ArgumentCaptor<PageRequest> pageRequestArg2 = ArgumentCaptor.forClass(PageRequest.class);
-
-        organizationsService.getOrganizationsBySearchAndPagination(pageNumber, itemsPerPage, search);
-
-        verify(mockOrganizationRepository).findByNameLikeIgnoreCase(anyString(), pageRequestArg2.capture());
-        assertEquals(pageRequest.first(), pageRequestArg.getValue().first());
-        assertEquals(pageRequest.first(), pageRequestArg2.getValue().first());
-        assertEquals(pageOrganizations, organizationsService
-                .getOrganizationsBySearchAndPagination(pageNumber, itemsPerPage, search));
-
-    }*/
+//        organizationService.getOrganizationsBySearchAndPagination(pageNumber, itemsPerPage, name, email,
+//                number, type, region, district, locality, streetToSearch, sortCriteria, sortOrder);
+    }
 
     @Test
     public void testGetOrganizationById() throws Exception {
-        final Long id = 100000L;
+        stub(organizationRepository.findOne(organizationId)).toReturn(organization);
+        Organization actual = organizationService.getOrganizationById(organizationId);
 
-        when(mockOrganizationRepository.findOne(id)).thenReturn(mockOrganization);
-//        ArgumentCaptor<Long> idArg = ArgumentCaptor.forClass(Long.class);
-
-        organizationsService.getOrganizationById(id);
-
-        assertEquals(mockOrganization, organizationsService.getOrganizationById(id));
-
+        assertEquals(organization, actual);
     }
 
-/*    @Test
     public void testEditOrganization() throws Exception {
-        final Long organizationId = 1L;
-        final String name = "name";
-        final String phone = "phone";
-        final String email = "email";
-        final String[] types = {"CALIBRATOR", "PROVIDER"};
-        final Integer employeesCapacity = 123;
-        final Integer maxProcessTime = 456;
-        final Address address = new Address("Lviv", "Leva", "123", "123", "123", "123");
-        final Organization organization = mock(Organization.class);
 
-        when(mockOrganizationRepository.findOne(organizationId)).thenReturn(organization);
-
-        ArgumentCaptor<String> nameArg = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<String> phoneArg = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<String> emailArg = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<Integer> employeesCapacityArg = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<Integer> maxProcessTimeArg = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<Address> addressArg = ArgumentCaptor.forClass(Address.class);
-
-        organizationsService.editOrganization(organizationId, name, phone, email, types,
-                employeesCapacity, maxProcessTime, address);
-
-        verify(organization).setName(nameArg.capture());
-        verify(organization).setPhone(phoneArg.capture());
-        verify(organization).setEmail(emailArg.capture());
-
-        verify(organization).setEmployeesCapacity(employeesCapacityArg.capture());
-        verify(organization).setMaxProcessTime(maxProcessTimeArg.capture());
-        verify(organization).setAddress(addressArg.capture());
-
-        assertEquals(name, nameArg.getValue());
-        assertEquals(phone, phoneArg.getValue());
-        assertEquals(email, emailArg.getValue());
-        assertEquals(employeesCapacity, employeesCapacityArg.getValue());
-        assertEquals(maxProcessTime, maxProcessTimeArg.getValue());
-        assertEquals(address.getBuilding(), addressArg.getValue().getBuilding());
-    }*/
-
-    /*@Test
-    public void testGetOrganizationEmployeesCapacity() throws Exception {
-        final Long organizationId = 1L;
-        Integer someInt = 123;
-        when(organizationsService.getOrganizationEmployeesCapacity(organizationId)).thenReturn(null);
-
-        ArgumentCaptor<Long> organizationIdArg = ArgumentCaptor.forClass(Long.class);
-
-        organizationsService.getOrganizationEmployeesCapacity(organizationId);
-
-        verify(organizationsService).getOrganizationEmployeesCapacity(organizationIdArg.capture());
-
-        assertEquals(organizationId, organizationIdArg.getValue());
-        assertEquals(someInt, organizationsService.getOrganizationEmployeesCapacity(organizationId));
-    }*/
+    }
 
     @Test
+    public void testGetOrganizationEmployeesCapacity() throws Exception {
+        stub(organization.getEmployeesCapacity()).toReturn(employeesCapasity);
+        stub(organizationRepository.findOne(organizationId)).toReturn(organization);
+        int actual = organizationService.getOrganizationEmployeesCapacity(organizationId);
+
+        assertEquals(employeesCapasity, actual);
+    }
+
     public void testSendOrganizationChanges() throws Exception {
-        mockMail.sendOrganizationChanges(mockOrganization, mockUser);
 
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void testGetHistoryByOrganizationId() throws Exception {
-        organizationsService.getHistoryByOrganizationId(null);
+        stub(organizationChangesHistoryRepository.findByOrganizationId(organizationId)).toReturn(historyList);
+        List<OrganizationChangesHistory> actual = organizationService.getHistoryByOrganizationId(organizationId);
+
+        assertEquals(historyList, actual);
     }
 
     @Test
     public void testFindAllByLocalityId() throws Exception {
-        Long id = 222L;
+        stub(organizationRepository.findOrganizationByLocalityId(localityId)).toReturn(organizationList);
+        List<Organization> actual = organizationService.findAllByLocalityId(localityId);
 
-        List<Organization> list = organizationsService.findAllByLocalityId(id);
-        assertNotNull(list);
+        assertEquals(organizationList, actual);
     }
 
     @Test
     public void testFindAllByLocalityIdAndTypeId() throws Exception {
-        Long id = 222L;
-        OrganizationType type = OrganizationType.NO_TYPE;
+        stub(organizationRepository.findOrganizationByLocalityIdAndType(localityId, orgType)).toReturn(organizationList);
+        List<Organization> actual = organizationService.findAllByLocalityIdAndTypeId(localityId, orgType);
 
-        List<Organization> list = organizationsService.findAllByLocalityIdAndTypeId(id, type);
-        assertNotNull(list);
+        assertEquals(organizationList, actual);
     }
 
     @Test
     public void testFindOrganizationTypesById() throws Exception {
-        Long id = 222L;
+        stub(organizationRepository.findOrganizationTypesById(organizationId)).toReturn(organizationTypes);
+        Set<OrganizationType> actual = organizationService.findOrganizationTypesById(organizationId);
 
-        Set<OrganizationType> set = organizationsService.findOrganizationTypesById(id);
-        assertNotNull(set);
+        assertEquals(organizationTypes, actual);
     }
+
 
     @Test
     public void testFindByLocalityIdAndTypeAndDevice() throws Exception {
-        Long localityId = 1L;
-        OrganizationType orgType = OrganizationType.CALIBRATOR;
-        DeviceType deviceType = DeviceType.WATER;
+        stub(organizationRepository.findByLocalityIdAndTypeAndDevice(localityId, orgType, deviceType)).toReturn(organizationList);
+        List<Organization> actual = organizationService.findByLocalityIdAndTypeAndDevice(localityId, orgType, deviceType);
 
-        List<Organization> list = organizationsService.findByLocalityIdAndTypeAndDevice(localityId, orgType, deviceType);
-        assertNotNull(list);
+        assertEquals(organizationList, actual);
     }
 }
