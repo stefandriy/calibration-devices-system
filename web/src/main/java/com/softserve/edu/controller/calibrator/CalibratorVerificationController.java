8ca24a44fcb497005f0fd13a0cb0b5ebbc7cc233
luckyvch <mobilede449@gmail.com>
2015-10-21 16:43:12 +0300

    fix bug in CalibratorVerificationController

--- web/src/main/java/com/softserve/edu/controller/calibrator/CalibratorVerificationController.java
+++ web/src/main/java/com/softserve/edu/controller/calibrator/CalibratorVerificationController.java
@@ -14,6 +14,7 @@ import com.softserve.edu.entity.enumeration.verification.Status;
 import com.softserve.edu.entity.organization.Organization;
 import com.softserve.edu.entity.user.User;
 import com.softserve.edu.entity.verification.Verification;
+import com.softserve.edu.entity.verification.calibration.AdditionalInfo;
 import com.softserve.edu.entity.verification.calibration.CalibrationTest;
 import com.softserve.edu.service.admin.OrganizationService;
 import com.softserve.edu.service.admin.UserService;
