78ce91c9da7973e9c6d6397ba96222c516ce84e3
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2015-10-23 07:49:16 +0300

    fix edit sys admin

--- web/src/main/webapp/resources/app/admin/controllers/SysAdminsController.js
+++ web/src/main/webapp/resources/app/admin/controllers/SysAdminsController.js
@@ -67,8 +67,7 @@ angular
                 $rootScope.username = username;
                 userService.getSysAdminByUsername(
                     $rootScope.username).then(
-                    function (data) {
-                        $rootScope.sysAdmin = data;
+                    function (data) {$rootScope.sysAdmin = data;
                         console.log($rootScope.sysAdmin);
 
                         var sysAdminDTOModal = $modal
