1b53e9cdf9ee14e021a61580c5e572cb1559e3e4
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2015-10-23 18:37:17 +0300

    start working with super_admin

--- web/src/main/java/com/softserve/edu/config/SecurityConfig.java
+++ web/src/main/java/com/softserve/edu/config/SecurityConfig.java
@@ -45,8 +45,8 @@ public class
                          "/application/**", "/calibrationTests/**" /*Some one has to move these tests out to verificator page!*/
                         , "/calibrationTestData/**").permitAll()
 
-                .antMatchers("/resources/app/admin/**", "/admin/**").hasAuthority("SYS_ADMIN")
-                
+                .antMatchers("/resources/app/admin/**", "/admin/**").hasAnyAuthority("SYS_ADMIN", "SUPER_ADMIN")
+
                 .antMatchers("/uploadFile/**").fullyAuthenticated()
                 
                 .antMatchers("/resources/app/**").hasAnyAuthority("PROVIDER_EMPLOYEE", "PROVIDER_ADMIN", "CALIBRATOR_EMPLOYEE", "CALIBRATOR_ADMIN", "STATE_VERIFICATOR_EMPLOYEE", "STATE_VERIFICATOR_ADMIN")
