ea1c6ed862b7084e6dcdffda276ac4dd99f37388
yurijdvornyk <yurijdvornyk@gmail.com>
2015-10-24 23:44:04 +0300

    Tests for calibrator and file operations added.

--- service/src/test/java/com/softserve/edu/service/storage/impl/FileOperationImplTest.java
+++ service/src/test/java/com/softserve/edu/service/storage/impl/FileOperationImplTest.java
@@ -1,22 +1,14 @@
 package com.softserve.edu.service.storage.impl;
 
-import com.softserve.edu.service.calibrator.impl.CalibratorPlaningTaskServiceImpl;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-
-import java.io.IOException;
 import java.net.URI;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Yurko on 23.10.2015.
+ * Created by Yurij Dvornyk on 21.10.2015.
  */
-@RunWith(MockitoJUnitRunner.class)
-@PrepareForTest(CalibratorPlaningTaskServiceImpl.class)
 public class FileOperationImplTest {
 
     @InjectMocks
