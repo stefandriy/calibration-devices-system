0cb7b3a9e2dc6b45bd9fcf8bb062e4e75b582b74
stefandriy <stefandriy@gmail.com>
2015-10-25 23:11:49 +0200

    updated: CalibrationTestDataServiceImplTest CalibrationTestServiceImplTest VerificationServiceImplTest

--- service/src/test/java/com/softserve/edu/service/calibrator/data/test/impl/CalibrationTestDataServiceImplTest.java
+++ service/src/test/java/com/softserve/edu/service/calibrator/data/test/impl/CalibrationTestDataServiceImplTest.java
@@ -1,9 +1,5 @@
 package com.softserve.edu.service.calibrator.data.test.impl;
 
-/**
- * Created by vova on 20.08.15.
- */
-
 import com.softserve.edu.entity.verification.calibration.CalibrationTestData;
 import com.softserve.edu.repository.CalibrationTestDataRepository;
 import org.junit.Assert;
