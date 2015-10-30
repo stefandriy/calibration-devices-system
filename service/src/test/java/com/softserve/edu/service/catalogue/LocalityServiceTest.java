958c6b7508a62bf66da0abb555e3e969ae649d91
Misha <admmisha@gmail.com>
2015-10-30 19:09:13 +0200

    «test»

--- service/src/test/java/com/softserve/edu/service/catalogue/LocalityServiceTest.java
+++ service/src/test/java/com/softserve/edu/service/catalogue/LocalityServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.*;
 
 import java.util.List;
 
@@ -17,29 +18,29 @@ import static org.junit.Assert.assertTrue;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ServiceTestingConfig.class})
 public class LocalityServiceTest {
-/*
+
     @Autowired
     private LocalityService localityService;
-
+    @Ignore
     @Test
     public void testFindOrganizationByLocalityId() {
         List<String> list = localityService.getMailIndexForLocality("Київ", 576L);
 
         assertTrue(!list.isEmpty());
     }
-
+    @Ignore
     @Test
     public void testGetLocalitiesCorrespondingDistrict() {
         List<Locality> list = localityService.getLocalitiesCorrespondingDistrict(1L);
 
         assertTrue(!list.isEmpty());
     }
-
+    @Ignore
     @Test
     public void testFindByDistrictIdAndOrganizationId() {
         List<Locality> list = localityService.findByDistrictIdAndOrganizationId(567L,1L);
 
         assertTrue(!list.isEmpty());
     }
-**/
+
 }
