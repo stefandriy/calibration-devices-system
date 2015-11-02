d9482091abc4ec8adbbddd9afb3f20b1be4cac48
yurijdvornyk <yurijdvornyk@gmail.com>
2015-11-02 13:26:00 +0200

    Measurement devices dictionary: created in admin

---
+++ web/src/main/webapp/resources/app/admin/controllers/MeasuringEquipmentControllerAdmin.js
@@ -0,0 +1,65 @@
+angular
+    .module('employeeModule')
+    .controller('MeasuringEquipmentControllerAdmin', ['$rootScope','$scope', '$modal','MeasuringEquipmentServiceAdmin', '$timeout',
+        function ($rootScope, $scope, $modal, equipmentServiceAdmin, $timeout) {
+            $scope.totalItems = 0;
+            $scope.currentPage = 1;
+            $scope.itemsPerPage = 5;
+            $scope.pageContent = [];
+       
+            /**
+             * Updates the table with MeasuringEquipments.
+             */
+            $rootScope.onTableHandling = function () {
+            	equipmentServiceAdmin
+					.getPage($scope.currentPage, $scope.itemsPerPage, $scope.searchData)
+            		.then(function (data) {
+						$scope.pageContent = data.content;
+						$scope.totalItems = data.totalItems;
+                });             
+            };
+            $rootScope.onTableHandling();
+            
+    		/**
+			 * Opens modal window for adding new equipment.
+			 */
+			$scope.openAddEquipmentModal = function() {
+				var addEquipmentModal = $modal
+						.open({
+							animation : true,
+							controller : 'MeasuringEquipmentAddModalControllerAdmin',
+							templateUrl : '/resources/app/admin/views/modals/measuring-equipment-add-modal.html',
+						});
+			};
+
+			/**
+			 * Opens modal window for editing equipment.
+			 */
+			$scope.openEditEquipmentModal = function(
+					equipmentId) {
+				$rootScope.equipmentId = equipmentId;
+				equipmentServiceAdmin.getEquipmentWithId(
+						$rootScope.equipmentId).then(
+						function(data) {
+							$rootScope.equipment = data;
+						});
+				var equipmentDTOModal = $modal
+						.open({
+							animation : true,
+							controller : 'MeasuringEquipmentEditModalControllerAdmin',
+							templateUrl : '/resources/app/admin/views/modals/measuring-equipment-edit-modal.html',
+						});
+			};
+			
+			$scope.deleteEquipment = function (equipmentId) {
+				$rootScope.equipmentId = equipmentId;
+				equipmentServiceAdmin.deleteEquipment(equipmentId);
+                $timeout(function() {
+                    console.log('delete with timeout');
+                    $rootScope.onTableHandling();
+                }, 700);
+
+            };
+			
+			
+    }]);
