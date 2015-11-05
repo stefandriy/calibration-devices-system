34eb21c0cfa39486109e1875e7dd8fc9c4420110
SofiaVynnytska <sonkavyn@gmail.com>
2015-11-05 10:03:19 +0200

    sentProtocols service

--- web/src/main/webapp/resources/app/calibrator/services/DigitalVerificationProtocolsServiceCalibrator.js
+++ web/src/main/webapp/resources/app/calibrator/services/DigitalVerificationProtocolsServiceCalibrator.js
@@ -1,17 +1,27 @@
 angular
     .module('employeeModule')
-    .factory('DigitalVerificationProtocolsServiceCalibrator',
-    function($http) {
+    .factory('DigitalVerificationProtocolsServiceCalibrator', ['$http', function ($http) {
         return {
-            getPage: function (pageNumber, itemsPerPage, search) {
+            getPage: function (pageNumber, itemsPerPage /*, search*/) {
                 var url = '/calibrator/protocols/' + pageNumber + '/' + itemsPerPage;
-                if (search != null && search != undefined && search != "")
-                    url += '/' + search;
+                /*  if (search != null && search != undefined && search != "")
+                 url += '/' + search;*/
 
                 return $http.get(url)
                     .then(function (result) {
                         return result.data;
                     });
             },
-        }
-    });
+            sendDataProtocol: function (url, data) {
+                return $http.put('calibrator/protocols/' + url, data)
+                    .success(function (responseData) {
+                        return responseData;
+                    })
+                    .error(function (err) {
+                        return err;
+                    });
+            }
+        };
+
+
+    }]);
