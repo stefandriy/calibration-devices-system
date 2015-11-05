7038b9f09f20edcdca263d1b1e049c675613f8a2
SofiaVynnytska <sonkavyn@gmail.com>
2015-11-05 23:39:19 +0200

    sentProtocols service

--- web/src/main/webapp/resources/app/calibrator/services/DigitalVerificationProtocolsServiceCalibrator.js
+++ web/src/main/webapp/resources/app/calibrator/services/DigitalVerificationProtocolsServiceCalibrator.js
@@ -10,19 +10,32 @@ angular
             },
             getProtocols: function (currentPage, itemsPerPage, search, sortCriteria, sortOrder) {
                 return getData('calibrator/protocols/' + currentPage + '/' + itemsPerPage + '/' + sortCriteria + '/' + sortOrder, search);
+            },
+            sendProtocols: function (protocol) {
+                return send('/protocols/send', protocol);
             }
-        };
+            };
 
 
         function getData(url, params) {
             $log.info(url);
-            return $http.get(url, {
+            return $http.get('/calibrator/protocols/'+url, {
                 params : params
             }).success(function (data) {
                 return data;
             }).error(function (err) {
                 return err;
             });
+        };
+        function send (url, protocol) {
+            return $http.post(url, protocol)
+                .success(function (data) {
+                    return data;
+                }).error(function (err) {
+                    console.log(err);
+                    return err;
+                });
         }
+
 
     }]);
