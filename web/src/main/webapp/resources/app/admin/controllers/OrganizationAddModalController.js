bd08db4400eb571003c34c4321cd149267a7025e
SofiaVynnytska <sonkavyn@gmail.com>
2015-11-10 11:27:52 +0200

    valid

--- web/src/main/webapp/resources/app/admin/controllers/OrganizationAddModalController.js
+++ web/src/main/webapp/resources/app/admin/controllers/OrganizationAddModalController.js
@@ -394,7 +394,7 @@ angular
 
 
 
-            $scope.ORGANIZATION_NAME_REGEX = /^[0-9A-Za-zА-ЯЄІЇҐ"'а-яєіїґ ]+$/;
+            $scope.ORGANIZATION_NAME_REGEX = /^[A-Za-zА-ЯЄІЇҐ"'а-яєіїґ ]+$/;
             $scope.PHONE_REGEX = /^[1-9]\d{8}$/;
             $scope.EMAIL_REGEX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
             $scope.FIRST_LAST_NAME_REGEX = /^([A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}\u002d{1}[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}|[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20})$/;
