87db437c018390ad46b6e08bdc0ebe57fac45e3f
yurijdvornyk <yurijdvornyk@gmail.com>
2015-11-12 00:00:25 +0200

    "$data set to null" - bug fixed!

--- web/src/main/webapp/resources/app/admin/controllers/MeasuringEquipmentControllerAdmin.js
+++ web/src/main/webapp/resources/app/admin/controllers/MeasuringEquipmentControllerAdmin.js
@@ -20,46 +20,51 @@ angular
             $scope.pageContent = [];
 
             /*$scope.organizationTypeData = [
-                {id: 'CALIBRATOR', label: null},
-                {id: 'PROVIDER', label: null},
-                {id: 'STATE_VERIFICATOR', label: null},
-                {id: 'NO_TYPE', label: null},
-            ];*/
+             {id: 'CALIBRATOR', label: null},
+             {id: 'PROVIDER', label: null},
+             {id: 'STATE_VERIFICATOR', label: null},
+             {id: 'NO_TYPE', label: null},
+             ];*/
 
             /*$scope.selectedOrganizationType = {
-                name: null
-            };*/
+             name: null
+             };*/
 
             /*$scope.setTypeDataLanguage();*/
 
             /*$scope.clearAll = function () {
-                $scope.tableParams.filter({});
-                $scope.selectedOrganizationType.name = null;
-            };*/
+             $scope.tableParams.filter({});
+             $scope.selectedOrganizationType.name = null;
+             };*/
 
             $scope.doSearch = function () {
                 $scope.tableParams.reload();
             };
 
             /*$scope.setTypeDataLanguage = function () {
-            };*/
+             };*/
 
             $scope.tableParams = new ngTableParams({
                 page: 1,
                 count: 5,
                 sorting: {
-                    id: 'desc'
+                    organizationId: 'desc'
                 }
             }, {
                 total: 0,
                 filterDelay: 1500,
                 getData: function ($defer, params) {
-                    /*if ($scope.selectedOrganizationType.name != null) {
-                        params.filter().type = $scope.selectedOrganizationType.name.id;
+                    if (params.settings().$scope == null) {
+                        params.settings().$scope = $scope;
                     }
-                    else {
-                        params.filter().type = null;//case when the filter is cleared with a button on the select
-                    }*/
+                    /*if ($scope.selectedOrganizationType.name != null) {
+                     params.filter().type = $scope.selectedOrganizationType.name.id;
+                     }
+                     else {
+                     params.filter().type = null;//case when the filter is cleared with a button on the select
+                     }*/
+
+                    params.filter().type = null;
 
                     var sortCriteria = Object.keys(params.sorting())[0];
                     var sortOrder = params.sorting()[sortCriteria];
@@ -86,25 +91,25 @@ angular
              * Opens modal window for adding new organization.
              */
             /*$scope.openAddOrganizationModal = function () {
-                var addOrganizationModal = $modal.open({
-                    animation: true,
-                    controller: 'OrganizationAddModalController',
-                    templateUrl: '/resources/app/admin/views/modals/organization-add-modal.html',
-                    size: 'lg',
-                    resolve: {
-                        regions: function () {
-                            return addressService.findAllRegions();
-                        }
-                    }
-                });*/
+             var addOrganizationModal = $modal.open({
+             animation: true,
+             controller: 'OrganizationAddModalController',
+             templateUrl: '/resources/app/admin/views/modals/organization-add-modal.html',
+             size: 'lg',
+             resolve: {
+             regions: function () {
+             return addressService.findAllRegions();
+             }
+             }
+             });*/
 
-                /**
-                 * executes when modal closing
-                 */
-                /*addOrganizationModal.result.then(function () {
-                    toaster.pop('success', $filter('translate')('INFORMATION'), $filter('translate')('SUCCESSFUL_CREATED_ORGANIZATION'));
-                });
-            };*/
+            /**
+             * executes when modal closing
+             */
+            /*addOrganizationModal.result.then(function () {
+             toaster.pop('success', $filter('translate')('INFORMATION'), $filter('translate')('SUCCESSFUL_CREATED_ORGANIZATION'));
+             });
+             };*/
 
             /**
              * Function for ng-show. When filtering fields are not empty show button for
@@ -125,52 +130,52 @@ angular
              * Opens modal window for editing organization.
              */
             /*$scope.openEditOrganizationModal = function (organizationId) {
-                $rootScope.organizationId = organizationId;
-                organizationService.getOrganizationWithId(
-                    $rootScope.organizationId).then(
-                    function (data) {
-                        $rootScope.organization = data;
-                        var organizationDTOModal = $modal
-                            .open({
-                                animation: true,
-                                controller: 'OrganizationEditModalController',
-                                templateUrl: '/resources/app/admin/views/modals/organization-edit-modal.html',
-                                size: 'lg',
-                                resolve: {
-                                    regions: function () {
-                                        return addressService.findAllRegions();
-                                    }
-                                }
-                            });
+             $rootScope.organizationId = organizationId;
+             organizationService.getOrganizationWithId(
+             $rootScope.organizationId).then(
+             function (data) {
+             $rootScope.organization = data;
+             var organizationDTOModal = $modal
+             .open({
+             animation: true,
+             controller: 'OrganizationEditModalController',
+             templateUrl: '/resources/app/admin/views/modals/organization-edit-modal.html',
+             size: 'lg',
+             resolve: {
+             regions: function () {
+             return addressService.findAllRegions();
+             }
+             }
+             });
 
-                        /!**
-                         * executes when modal closing
-                         *!/
-                        organizationDTOModal.result.then(function () {
-                            toaster.pop('info', $filter('translate')('INFORMATION'), $filter('translate')('SUCCESSFUL_EDITED_ORGANIZATION'));
-                        });
-                    });
+             /!**
+             * executes when modal closing
+             *!/
+             organizationDTOModal.result.then(function () {
+             toaster.pop('info', $filter('translate')('INFORMATION'), $filter('translate')('SUCCESSFUL_EDITED_ORGANIZATION'));
+             });
+             });
 
-            };*/
+             };*/
 
             /**
              * Opens modal window for show history for all organization changes.
              */
             /*$scope.openOrganizationEditHistoryModal = function (organizationId) {
-                $rootScope.organizationId = organizationId;
-                organizationService.getHistoryOrganizationWithId(
-                    organizationId).then(
-                    function (data) {
-                        $rootScope.organization = data.content;
-                        var organizationDTOModal = $modal
-                            .open({
-                                animation: true,
-                                controller: 'OrganizationEditHistoryModalController',
-                                templateUrl: '/resources/app/admin/views/modals/organization-edit-history-modal.html',
-                                size: 'lg'
-                            });
-                    });
+             $rootScope.organizationId = organizationId;
+             organizationService.getHistoryOrganizationWithId(
+             organizationId).then(
+             function (data) {
+             $rootScope.organization = data.content;
+             var organizationDTOModal = $modal
+             .open({
+             animation: true,
+             controller: 'OrganizationEditHistoryModalController',
+             templateUrl: '/resources/app/admin/views/modals/organization-edit-history-modal.html',
+             size: 'lg'
+             });
+             });
 
-            };*/
+             };*/
 
         }]);
