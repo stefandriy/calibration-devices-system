222f921cf762162a8ad92498c28ac824cdd357a1
Veronichka <herasymenkoveronika@gmail.com>
2015-11-11 23:54:54 +0100

    the rest-method for button Send

--- web/src/main/java/com/softserve/edu/controller/calibrator/DigitalVerificationProtocolsCalibratorController.java
+++ web/src/main/java/com/softserve/edu/controller/calibrator/DigitalVerificationProtocolsCalibratorController.java
@@ -1,21 +1,21 @@
 package com.softserve.edu.controller.calibrator;
 
 import com.softserve.edu.controller.calibrator.util.ProtocolDTOTransformer;
+import com.softserve.edu.dto.VerificationUpdateDTO;
 import com.softserve.edu.dto.calibrator.ProtocolDTO;
 import com.softserve.edu.entity.enumeration.verification.Status;
 import com.softserve.edu.dto.NewVerificationsFilterSearch;
 import com.softserve.edu.dto.PageDTO;
+import com.softserve.edu.entity.organization.Organization;
 import com.softserve.edu.entity.user.User;
 import com.softserve.edu.entity.verification.Verification;
 import com.softserve.edu.service.calibrator.CalibratorDigitalProtocolsService;
 import com.softserve.edu.service.calibrator.CalibratorEmployeeService;
 import com.softserve.edu.service.user.SecurityUserDetailsService;
+import com.softserve.edu.service.verification.VerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +31,9 @@ public class DigitalVerificationProtocolsCalibratorController {
 
     @Autowired
     CalibratorEmployeeService calibratorEmployeeService;
+
+    @Autowired
+    VerificationService verificationService;
 
     /**
      * This method calls service whiche returns the list of verifications. The controller transform them with the help of
@@ -56,9 +59,17 @@ public class DigitalVerificationProtocolsCalibratorController {
                     calibratorEmployee,pageNumber, itemsPerPage, status );
             Long count = protocolsService.countByCalibratorEmployee_usernameAndStatus(calibratorEmployee, status);
             List<ProtocolDTO> content = ProtocolDTOTransformer.toDtofromList(verifications);
-//            Long count = Long.valueOf(content.size());
 
             return new PageDTO<>(count,content);
 
+    }
+
+    @RequestMapping(value="send", method = RequestMethod.PUT)
+    public void updateVerification(@RequestBody VerificationUpdateDTO verificationUpdateDTO) {
+        for(String verificationId : verificationUpdateDTO.getIdsOfVerifications()) {
+            Long idVerificator = verificationUpdateDTO.getOrganizationId();// TODO: ask about verificatorOrganization
+            Organization verificator = verificatorService.findById(idVerificator);//TODO: write in StateVerificatorServiceImpl method findById() which simply calls repository
+            verificationService.sendVerificationTo(verificationId, verificator, Status.SENT_TO_VERIFICATOR);
+        }
     }
 }
