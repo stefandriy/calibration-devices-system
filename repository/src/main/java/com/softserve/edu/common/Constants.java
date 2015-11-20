068cf5b21e7563372a292719acbfe20ddea82f21
SofiaVynnytska <sonkavyn@gmail.com>
2015-11-20 11:20:42 +0200

    commom

--- repository/src/main/java/com/softserve/edu/common/Constants.java
+++ repository/src/main/java/com/softserve/edu/common/Constants.java
@@ -4,5 +4,5 @@ package com.softserve.edu.common;
  * Created by Sonka on 20.11.2015.
  */
 public interface Constants {
-     String IMAGE_TYPE = "jpg"; // remve from here. mov to the separate interface
+     String IMAGE_TYPE = "jpg";
 }
