a67f8a80033ce45944b69d0ec355dc8a884a8bb8
lubomyr <lubomyr31@gmail.com>
2015-11-23 18:51:26 +0200

    Added additional info fields to planing task table

--- web/src/main/java/com/softserve/edu/dto/calibrator/VerificationPlanningTaskDTO.java
+++ web/src/main/java/com/softserve/edu/dto/calibrator/VerificationPlanningTaskDTO.java
@@ -4,6 +4,7 @@ import com.softserve.edu.entity.device.Device;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalTime;
 import java.util.Date;
 
 @Getter
@@ -11,34 +12,30 @@ import java.util.Date;
 public class VerificationPlanningTaskDTO {
 
     private Date sentToCalibrator;
-
     private String verficationId;
-
     private String providerName;
-
     private String clientFullName;
-
     private String district;
-
     private String street;
-
     private String building;
-
     private String flat;
-
     private String telephone;
-
     private String secondphone;
-
     private String building_flat;
-
     private String phone;
+    private String time;
+    private Date dateOfVerif;
+    private LocalTime timeFrom;
+    private LocalTime timeTo;
+    private String serviceability;
+    private Date noWaterToDate;
 
     public VerificationPlanningTaskDTO(){}
 
     public VerificationPlanningTaskDTO(Date sentDate, String verificationID, String providerName, String fullName,
                                        String district, String street, String building, String flat, String telephone,
-                                       String secondphone){
+                                       String secondphone, Date dateOfVerif, LocalTime timeFrom, LocalTime timeTo,
+                                       boolean serviceability, Date noWaterToDate){
         this.sentToCalibrator = sentDate;
         this.verficationId = verificationID;
         this.providerName = providerName;
@@ -49,6 +46,23 @@ public class VerificationPlanningTaskDTO {
         this.flat = flat;
         this.telephone = telephone;
         this.secondphone = secondphone;
+        this.dateOfVerif = dateOfVerif;
+        this.noWaterToDate = noWaterToDate;
+
+        if (serviceability)
+            this.serviceability = "Так";
+        else
+            this.serviceability = "Ні";
+
+        if ((timeFrom != null) || (timeTo != null))
+        {
+            this.time = timeFrom.toString() + " - " + timeTo.toString();
+        }
+        else
+        {
+            this.time = null;
+        }
+
         if ((flat != null) && !flat.isEmpty())
         {
             this.building_flat = building+"  /  "+flat;
@@ -57,6 +71,7 @@ public class VerificationPlanningTaskDTO {
         {
             this.building_flat = building;
         }
+
         if ((secondphone != null) && !secondphone.isEmpty())
         {
             this.phone = telephone+", "+secondphone;
