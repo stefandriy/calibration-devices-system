b1fe0d2379e5a66714a603863451b90e1251b446
Veronichka <herasymenkoveronika@gmail.com>
2015-12-13 19:59:36 +0100

    Deleted import from OrganizationStageVerificationDTO

--- web/src/main/java/com/softserve/edu/dto/provider/OrganizationStageVerificationDTO.java
+++ web/src/main/java/com/softserve/edu/dto/provider/OrganizationStageVerificationDTO.java
@@ -1,13 +1,8 @@
 package com.softserve.edu.dto.provider;
 
 import com.softserve.edu.entity.Address;
-import com.softserve.edu.entity.device.Counter;
-import com.softserve.edu.entity.organization.Organization;
 import com.softserve.edu.entity.verification.ClientData;
 
-import com.softserve.edu.entity.verification.calibration.AdditionalInfo;
-import com.sun.deploy.util.SessionState;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -84,48 +79,6 @@ public class OrganizationStageVerificationDTO {
 		this.verificationId = verificationId;
 	}
 
-//	public OrganizationStageVerificationDTO(ClientData clientData, Address address, String verificationId,
-//											Organization calibrator, String comment, AdditionalInfo info,
-//											Boolean dismantled, Counter counter) {
-//		this.firstName = clientData.getFirstName();
-//		this.lastName = clientData.getLastName();
-//		this.middleName = clientData.getMiddleName();
-//		this.email = clientData.getEmail();
-//		this.phone = clientData.getPhone();
-//		this.secondPhone = clientData.getSecondPhone();
-//		this.region = address.getRegion();
-//		this.locality = address.getLocality();
-//		this.district = address.getDistrict();
-//		this.street = address.getStreet();
-//		this.building = address.getBuilding();
-//		this.flat = address.getFlat();
-//
-//		this.verificationId = verificationId;
-//		this.calibratorName = (calibrator != null) ? calibrator.getName() : null;
-//		this.comment = comment;
-//
-//		this.entrance = (info != null) ? "" + info.getEntrance() : null;
-//		this.doorCode = (info != null) ? "" + info.getDoorCode() : null;
-//		this.floor = (info != null) ? "" + info.getFloor() : null;
-//		this.dateOfVerif = (info != null && info.getDateOfVerif() != null) ? info.getDateOfVerif().getTime() : null;
-//		this.serviceability = (info != null) ? info.getServiceability() : null;
-//		this.noWaterToDate = (info != null && info.getNoWaterToDate() != null) ? info.getNoWaterToDate().getTime() : null;
-//		this.notes = (info != null) ? info.getNotes() : null;
-//		this.time = (info != null) ? info.getTimeFrom() + "-" + info.getTimeTo() : null;
-//
-//		this.dismantled = dismantled;
-//
-//		this.dateOfDismantled = ( counter != null && counter.getDateOfDismantled() != null) ?
-//				counter.getDateOfDismantled().getTime() : null;
-//		this.dateOfMounted = (counter != null && counter.getDateOfMounted() != null) ?
-//				counter.getDateOfMounted().getTime() : null;
-//		this.numberCounter = (counter != null) ? counter.getNumberCounter() : null;
-//		this.releaseYear = (counter != null) ? counter.getReleaseYear() : null;
-//
-//		this.symbol = (counter != null && counter.getCounterType() != null) ? counter.getCounterType().getSymbol() : null;
-//		this.standardSize = (counter != null && counter.getCounterType() != null) ?
-//				counter.getCounterType().getStandardSize() : null;
-//	}
 
 	public OrganizationStageVerificationDTO(ClientData clientData, Address address, String verificationId, String calibratorName,
 											String entrance, String doorCode, String floor, Long dateOfVerif, Boolean serviceability,
